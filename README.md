0ca3bbc5e656d14a5181b0ec02a7cabd067b638c
EyeStye <ayushthe8055@gmail.com>
2024-12-18 11:21:16 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # test
-Welcome to my first project😊. This will keep receiving updates. Currently it is **version 0.0**.<br/>
-[View it by clicking me](https://eyestye.github.io/test)
-## Version 0.0
-***Pre-released*** and very basic. Enter date, month and year of birth in 3 _different_ prompts and calculate age, it does not account for leap year and takes 1 month as 30 days.
 
+Welcome to my first project😊. This will keep receiving updates. Currently it is **version 0.5**.<br/>
+[View it by clicking me](https://eyestye.github.io/test)
+
+## Version 0.0
+
+**_Pre-released_** and very basic. Enter date, month and year of birth in 3 _different_ prompts and calculate age (from a _fixed_ date), it does not account for leap year and takes 1 month as 30 days.
+
+## Version 0.5
+
+- Enter the date of birth in a **single** prompt.
+- Now the calculator calculates from today's date (UTC+0)
+- It displays singular and plural words accordingly (like using year for years when it is 1, etc.)
+- See the message when you enter today's date as date of birth!
--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-//age(in years,months and days) calculator
-const date = Number(prompt('Enter your date of birth'));
-const month = Number(prompt('Enter your month of birth'));
-const year = Number(prompt('Enter your year of birth'));
-const birthDate = [date, month, year];
-const curDate = [13, 12, 2024];
+const date = prompt("Enter your date of birth in DD-MM-YYYY format");
+const birthDate = [date.slice(0, 2), date.slice(3, 5), date.slice(6, 10)];
+// fetch current date on basis of UTC +0
+let today = new Date().toISOString().slice(0, 10); //YYYY-MM-DD
+const curDate = [today.slice(8, 10), today.slice(5, 7), today.slice(0, 4)];
 const age = function (dob, cd) {
   const ageInDays = [];
   for (let i = 0; i < 3; i++) {
-    ageInDays.push(cd[i] - dob[i]);
+    ageInDays.push(Number(cd[i]) - Number(dob[i]));
   }
-
   if (ageInDays[0] < 0) {
     ageInDays[0] += 30;
     ageInDays[1]--;
@@ -18,12 +16,24 @@ const age = function (dob, cd) {
     ageInDays[1] += 12;
     ageInDays[2]--;
   }
+  terms = ["day", "month", "year"];
+  for (let i = 0; i < terms.length; i++) {
+    terms[i] = ageInDays[i] !== 1 ? terms[i] + "s" : terms[i];
+  }
+  if (ageInDays[2] === 0 && ageInDays[1] === 0 && ageInDays[0] === 0)
+    return `You are just born today 🎉`;
   if (ageInDays[1] === 0 && ageInDays[0] === 0)
-    return `You are ${ageInDays[2]} years old😀`;
+    return `You are ${ageInDays[2]} ${terms[2]} old 😀`;
+  if (ageInDays[2] === 0 && ageInDays[0] === 0)
+    return `You are ${ageInDays[1]} ${terms[1]} old 😀`;
+  if (ageInDays[1] === 0 && ageInDays[2] === 0)
+    return `You are ${ageInDays[0]} ${terms[0]} old 😀`;
   if (ageInDays[1] === 0)
-    return `You are ${ageInDays[2]} years, ${ageInDays[0]} days old😀`;
+    return `You are ${ageInDays[2]} ${terms[2]}, ${ageInDays[0]} ${terms[0]} old 😀`;
   if (ageInDays[0] === 0)
-    return `You are ${ageInDays[2]} years, ${ageInDays[1]} months old😀`;
-  return `You are ${ageInDays[2]} years, ${ageInDays[1]} months, ${ageInDays[0]} days old😀`;
+    return `You are ${ageInDays[2]} ${terms[2]}, ${ageInDays[1]} ${terms[1]} old 😀`;
+  if (ageInDays[2] === 0)
+    return `You are ${ageInDays[1]} ${terms[1]}, ${ageInDays[0]} ${terms[0]} old 😀`;
+  return `You are ${ageInDays[2]} ${terms[2]}, ${ageInDays[1]} ${terms[1]}, ${ageInDays[0]} ${terms[0]} old 😀`;
 };
 alert(age(birthDate, curDate));
